1eaa514c5b2f9a5fcfe1cc62290b28bd373b1af0
Avnish Kumar <avnish015@gmail.com>
2024-03-20 00:48:26 +0530

    Initial Commit

---
+++ CiCD_Sample/CiCD_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCD_SampleApp.swift
+//  CiCD_Sample
+//
+//  Created by Nishant Kumar on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CiCD_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CiCD_Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CiCD_Sample
+//
+//  Created by Nishant Kumar on 20/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
